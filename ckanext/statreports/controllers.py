7404edfde62a32afef8bfd523332ff07fade2a68
Peter Kronström <peter.kronstrom@gofore.com>
2015-05-06 13:55:52 +0300

    850, Add a new statistics page to show basic information on site users and packages

---
+++ ckanext/statreports/controllers.py
@@ -0,0 +1,32 @@
+from ckan.lib.base import BaseController, render
+from ckanext.statreports.statistics.user import UserStats
+from ckanext.statreports.statistics.package import PackageStats
+from ckan.model.meta import engine
+
+class StatisticsController(BaseController):
+    '''
+    StatisticsController renders a simple stat page
+    which can be used to keep track on various Etsin
+    statistics.
+    '''
+
+    def render_stats(self):
+        '''
+        Renders the stats page
+        '''
+
+        # Save the user stats in extra_vars variable, which is then passed on to the template
+        extra_vars = {}
+
+        extra_vars["total_users"] = UserStats.total_users()
+        extra_vars["total_visitors"] = UserStats.total_visitors(engine)
+        extra_vars["total_logged_in"] = UserStats.total_logged_in(engine)
+        extra_vars["total_packages"] = PackageStats.total_packages()
+
+        packages_by_license_type = PackageStats.license_type_package_count()
+
+        extra_vars["packages_free"] = packages_by_license_type["free"]
+        extra_vars["packages_conditional"] = packages_by_license_type["conditional"]
+        extra_vars["packages_other"] = packages_by_license_type["other"]
+
+        return  render('statreports/stats.html', extra_vars=extra_vars)
--- ckanext/statreports/plugin.py
+++ ckanext/statreports/plugin.py
@@ -1,5 +1,21 @@
 import ckan.plugins as p
-
+import ckan.plugins.toolkit as toolkit
 
 class StatReports(p.SingletonPlugin):
-    pass
+
+    p.implements(p.IConfigurer, inherit=True)
+    p.implements(p.IRoutes, inherit=True)
+
+    def update_config(self, config):
+        toolkit.add_template_directory(config, 'templates')
+        toolkit.add_public_directory(config, 'public')
+
+
+    def before_map(self, map):
+
+        map.connect('statreports',
+                    '/statistics',
+                    controller='ckanext.statreports.controllers:StatisticsController',
+                    action='render_stats')
+
+        return map
